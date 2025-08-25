27d67b7e9462e4b9732dd9c692f70aa080d80ee2
DIRrinn <cosmologicalhorizon@yandex.ru>
2025-08-25 13:21:50 +0700

    Через ООП и панели

--- tui.py
+++ tui.py
@@ -1,58 +1,102 @@
 import curses
-import curses.textpad
+from curses import panel
+import download
+import set_config
+import convert
 
-# Основной экран
-def screen():
-    global screen
-    screen = curses.initscr()
-    curses.start_color()
+class MenuItem(object):
+    def __init__(self, items, stdscreen):
+        self.size_x = curses.COLS // 2
+        self.size_y = curses.LINES // 2
 
-    # Цвета
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_WHITE)
-    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_WHITE)
+        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        curses.start_color()
 
-    curses.noecho()  # Выкл сообщений в консоль
-    curses.cbreak()
-    curses.curs_set(0)
-    screen.box(0, 0)  # Границы как и screen.border(0)
-    screen.addstr(0, 0, "Curtube by DIRrinn ", curses.A_DIM)
+        self.logo = stdscreen.subwin(self.size_y, self.size_x, self.size_y - self.size_y // 2, self.size_x - self.size_x // 2)
+        self.logo.keypad(1)
+        self.logo.bkgdset(' ', curses.color_pair(1) | curses.A_REVERSE)
 
-    # Размер окна
-    lines = curses.LINES  # y
-    cols = curses.COLS    # x
-    win_size_y = lines // 2
-    win_size_x = cols // 2
-    screen.refresh() # Обновление экрана
-
-    # Меню (y, x)
-    menu = curses.newwin(win_size_y, win_size_x, win_size_y - win_size_y // 2, win_size_x - win_size_x // 2)
-    menu.bkgd(' ', curses.color_pair(1) | curses.A_BOLD | curses.A_REVERSE)
-
-    # Лого
-    menu.addstr(1, 0, r'''
+        self.logo.addstr(1, 0, r'''
     ____ _   _ ____ _____ _   _ ____  _____
    / ___| | | |  _ \_   _| | | | __ )| ____|
   | |   | | | | |_) || | | | | |  _ \|  _|
   | |___| |_| |  _ < | | | |_| | |_) | |___
    \____|\___/|_| \_\|_|  \___/|____/|_____|
 ''')
+        self.logo.box(0, 0)
 
-    menu.box(0, 0)
+        self.window = stdscreen.subwin(self.size_y // 2, self.size_x, self.size_y, self.size_x - self.size_x // 2)
+        self.window.keypad(1)
+        self.window.bkgdset(' ', curses.color_pair(1))
 
-    # Текст в меню (кнопки) - Заготовка. Сделать через curses.textpad.Textbox
-    buttons = ["Config", "Set url", "Download", "Exit"]
-    pos = 10
-    for i in buttons:
-        menu.addstr(pos, 3, i, curses.A_BOLD)
-        pos += 2
+        self.panel = panel.new_panel(self.window)
+        self.panel.hide()
+        panel.update_panels()
 
-    menu.refresh()
+        self.position = 0
+        self.items = items
+        self.items.append(("Exit", "Exit"))
 
-    screen.getch()
+    def navigate(self, n):
+        self.position += n
+        if self.position < 0:
+            self.position = 0
+        elif self.position >= len(self.items):
+            self.position = len(self.items) - 1
 
-screen()
+    def display(self):
+        self.panel.top()
+        self.panel.show()
 
-# Окончание работы с окном
-curses.nocbreak()
-curses.echo()
-curses.endwin()
+        while True:
+            self.window.refresh()
+            curses.doupdate()
+
+            for index, item in enumerate(self.items):
+                if index == self.position:
+                    mode = curses.A_REVERSE
+                else:
+                    mode = curses.A_NORMAL
+
+                msg = "%d. %s" % (index, item[0])
+                self.window.addstr(1 + index, 1, msg, mode)
+
+            key = self.window.getch()
+
+            if key in [curses.KEY_ENTER, ord("\n")]:
+                if self.position == len(self.items) - 1:
+                    return 0
+                else:
+                    self.items[self.position][1]()
+
+            elif key == curses.KEY_UP:
+                self.navigate(-1)
+
+            elif key == curses.KEY_DOWN:
+                self.navigate(1)
+
+        self.window.clear()
+        self.panel.hide()
+        panel.update_panels()
+        curses.doupdate()
+
+class App(object):
+    def __init__(self, stdscreen):
+        self.screen = stdscreen
+        self.screen.box(0, 0)  # Границы как и screen.border(0)
+        self.screen.addstr(0, 0, "Curtube by DIRrinn ", curses.A_DIM)
+
+        curses.curs_set(0)
+
+        menu_items = [
+            ("Config", set_config.configuration),
+            ("Set Url", set_config.set_url),
+            ("Download", download.download)
+        ]
+        menu = MenuItem(menu_items, self.screen)
+        menu.display()
+
+
+if __name__ == "__main__":
+    curses.wrapper(App)
