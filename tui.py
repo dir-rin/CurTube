12f4d6f4d0385cd8e82b29f3b89e5f1f4b7b515e
DIRrinn <cosmologicalhorizon@yandex.ru>
2025-08-26 20:52:24 +0700

    Better now

--- tui.py
+++ tui.py
@@ -1,38 +1,30 @@
 import curses
-from curses import panel
 import download
 import set_config
 import convert
+from popup_window import PopupWindow
 
 class MenuItem(object):
-    def __init__(self, items, stdscreen):
+    def __init__(self, items):
         self.size_x = curses.COLS // 2
         self.size_y = curses.LINES // 2
 
-        curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_WHITE)
-        curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
-        curses.start_color()
+        self.window = curses.newwin(self.size_y, self.size_x, self.size_y - self.size_y // 2, self.size_x - self.size_x // 2)
+        self.window.keypad(1)
+        self.window.bkgd(' ', curses.color_pair(1))
 
-        self.logo = stdscreen.subwin(self.size_y, self.size_x, self.size_y - self.size_y // 2, self.size_x - self.size_x // 2)
-        self.logo.keypad(1)
-        self.logo.bkgdset(' ', curses.color_pair(1) | curses.A_REVERSE)
-
-        self.logo.addstr(1, 0, r'''
+        self.window.addstr(1, 0, r'''
     ____ _   _ ____ _____ _   _ ____  _____
    / ___| | | |  _ \_   _| | | | __ )| ____|
   | |   | | | | |_) || | | | | |  _ \|  _|
   | |___| |_| |  _ < | | | |_| | |_) | |___
    \____|\___/|_| \_\|_|  \___/|____/|_____|
+
+   A small TUI app to download Youtube videos.
+
+   Move with arrows. Enter to choose option.
 ''')
-        self.logo.box(0, 0)
-
-        self.window = stdscreen.subwin(self.size_y // 2, self.size_x, self.size_y, self.size_x - self.size_x // 2)
-        self.window.keypad(1)
-        self.window.bkgdset(' ', curses.color_pair(1))
-
-        self.panel = panel.new_panel(self.window)
-        self.panel.hide()
-        panel.update_panels()
+        self.window.box(0, 0)
 
         self.position = 0
         self.items = items
@@ -46,8 +38,6 @@ class MenuItem(object):
             self.position = len(self.items) - 1
 
     def display(self):
-        self.panel.top()
-        self.panel.show()
 
         while True:
             self.window.refresh()
@@ -60,7 +50,7 @@ class MenuItem(object):
                     mode = curses.A_NORMAL
 
                 msg = "%d. %s" % (index, item[0])
-                self.window.addstr(1 + index, 1, msg, mode)
+                self.window.addstr(15 + index, 1, msg, mode)
 
             key = self.window.getch()
 
@@ -77,16 +67,18 @@ class MenuItem(object):
                 self.navigate(1)
 
         self.window.clear()
-        self.panel.hide()
-        panel.update_panels()
         curses.doupdate()
-
+        
 class App(object):
     def __init__(self, stdscreen):
         self.screen = stdscreen
         self.screen.box(0, 0)  # Границы как и screen.border(0)
         self.screen.addstr(0, 0, "Curtube by DIRrinn ", curses.A_DIM)
+        self.screen.refresh()
 
+        # COLORS
+        curses.start_color()
+        curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_CYAN)
         curses.curs_set(0)
 
         menu_items = [
@@ -94,8 +86,8 @@ class App(object):
             ("Set Url", set_config.set_url),
             ("Download", download.download)
         ]
-        menu = MenuItem(menu_items, self.screen)
-        menu.display()
+        self.menu = MenuItem(menu_items)
+        self.menu.display()
 
 
 if __name__ == "__main__":
