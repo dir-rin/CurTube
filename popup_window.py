b84d2343b902ea61c235e1b8ee6737d2267a6b6e
dir-rin <cosmologicalhorizon@yandex.ru>
2025-08-29 14:31:27 +0700

    Now it's working

--- popup_window.py
+++ popup_window.py
@@ -4,6 +4,7 @@ class PopupWindow(object):
     def __init__(self, pos, label):
         self.window = curses.newwin(10, 20, pos[0], pos[1])
         self.window.box(0, 0)
+        self.window.keypad(1)
         self.window.addstr(0, 1, label)
         self.position = 0
 
@@ -21,34 +22,71 @@ class PopupWindow(object):
         self.position += n
         if self.position < 0:
             self.position = 0
-        elif self.position >= len(self.buttons):
-            self.position = len(self.buttons) - 1
+        elif self.position >= len(self.options):
+            self.position = len(self.options) - 1
 
     def display(self):
-        if self.buttons != None:
-            pos = 2
-            for index, item in enumerate(self.buttons):
-                if index == self.position:
-                    mode = curses.A_REVERSE
-                else:
-                    mode = curses.A_NORMAL
+        if self.buttons != ():
+            while True:
+                pos = 2
+                for index, item in enumerate(self.buttons):
+                    self.window.refresh()
+                    curses.doupdate()
 
-                self.window.addstr(8, pos, index + 1 + ". " + item[0], mode)
-                pos += 10
+                    if index == self.position:
+                        mode = curses.A_REVERSE
+                    else:
+                        mode = curses.A_NORMAL
 
-            self.window.refresh()
+                    self.window.addstr(8, pos, item, mode)
+                    pos += 10
 
-        if self.options != None:
-            pos = 3
-            for index, item in enumerate(self.options):
-                if index == self.position:
-                    mode = curses.A_REVERSE
-                else:
-                    mode = curses.A_NORMAL
+                    key = self.window.getch()
 
-                self.window.addstr(pos + index, pos, item[0], mode)
+                    if key in [curses.KEY_ENTER, ord("\n")]:
+                        if self.position == len(self.buttons) - 1:
+                            return 0
+                        else:
+                            self.buttons[self.position][1]()
 
-            self.window.refresh()
+                    elif key == curses.KEY_LEFT:
+                        self.navigation(-1)
+                    elif key == curses.KEY_RIGHT:
+                        self.navigation(1)
+
+            self.window.clear()
+            curses.doupdate()
+
+
+        elif self.options != ():
+            while True:
+                self.window.refresh()
+                curses.doupdate()
+
+                pos = 4
+                for index, item in enumerate(self.options):
+                    if index == self.position:
+                        mode = curses.A_REVERSE
+                    else:
+                        mode = curses.A_NORMAL
+
+                    self.window.addstr(pos + index, pos, item[0], mode)
+
+                key = self.window.getch()
+
+                if key in [curses.KEY_ENTER, ord("\n")]:
+                    if self.position == len(self.options) - 1:
+                        return 0
+                    else:
+                        self.options[self.position][1]()
+
+                elif key == curses.KEY_UP:
+                    self.navigation(-1)
+                elif key == curses.KEY_DOWN:
+                    self.navigation(1)
+
+            self.window.clear()
+            curses.doupdate()
 
     def addstr(self, pos, string):
         self.window.addstr(pos[0], pos[1], string)
